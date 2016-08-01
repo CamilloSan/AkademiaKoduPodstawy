89bb830500c6dbe047be7b77be1d649be262c8a5
KS <camill100@gmail.com>
2016-08-01 20:33:24 +0200

    01

---
+++ JavaPodstawy/src/podstawy/Zad1.java
@@ -0,0 +1,31 @@
+package podstawy;
+
+import java.util.Scanner;
+
+public class Zad1 {
+
+	// Napisz program, który deklaruje 2 zmienne ca³kowite i na ekran wypisuje
+	// ich sumê jj
+
+	public static void main(String[] args) {
+
+		int a, b;
+		System.out.println("Program dodaje 2 liczby(int).");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Podaj liczbe a?");
+		a = sc.nextInt();
+		System.out.println("Podaj liczbe b?");
+		b = sc.nextInt();
+		System.out.println("---------------");
+		// dodanie 2 zmiennych
+		System.out.println("dobra forma (a + b)");
+		System.out.println("Suma 2 liczb to: " + (a + b));
+		System.out.println("---------------");
+		// wynik = wypisanie wartoœci zmienych jako String a i b (1 i 2 = 12),
+		// nie dodaje sie w taki spsób
+		System.out.println("z³¹ forma  a + b");
+		System.out.println("Suma 2 liczb to: " + a + b);
+
+	}
+
+}
